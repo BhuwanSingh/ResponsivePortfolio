fa0787ba45e89094736e2f3c6df9284fb2e6ab3c
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-10-27 13:37:20 +0530

    intro complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,6 @@
 import './intro.css'
+import me from '../../img/me.png'
+import scroll from '../../img/scroll.svg'
 
 const Intro = () => {
   return (
@@ -9,18 +11,27 @@ const Intro = () => {
           <h1 className="i-name">Bhuwan Singh</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
-              <div className="i-title-wrapper">
-                <div className="i-title-item">Web Developer</div>
-                <div className="i-title-item">UI/UX Designer</div>
-                <div className="i-title-item">Competitive Programmer</div>
-                <div className="i-title-item">Student</div>
-                <div className="i-title-item">Content Creator</div>
-              </div>
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">UI/UX Designer</div>
+              <div className="i-title-item">Programmer</div>
+              <div className="i-title-item">Student</div>
+              <div className="i-title-item">Content Creator</div>
             </div>
           </div>
+          <div className="i-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            nulla corporis adipisci illo magni amet nesciunt, quas minus,
+            quibusdam vel libero exercitationem repudiandae sit laborum eius
+            sunt deleniti, mollitia cupiditate?
+          </div>
+        </div>
+        <img src={scroll} alt="scroll" className="i-scroll" />
+      </div>
+      <div className="i-right">
+        <div className="i-bg">
+          <img src={me} alt="me" className="i-img" />
         </div>
       </div>
-      <div className="i-right">right</div>
     </div>
   )
 }
